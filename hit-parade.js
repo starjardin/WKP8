414acb768581cb631e4968389730320d0273f86f
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-22 15:06:56 +0300

    added list of songs to local storage

--- hit-parade.js
+++ hit-parade.js
@@ -6,37 +6,11 @@ Plan:
 4-Make a function that sorts the list from the most popular to the lest popular
 5-Make a function that handles the update
 6-Make a function that delete the list
+7-Grab the inputs for the search and listen for type input
+8-compare the value of the input to the title and the style
 */
 
-let songs = [
-  {
-    title: "Perfect",
-    name: "Ed Sheeran",
-    style: "Pop",
-    length: "4:04",
-    picuture: "ed-sheeran.jpg",
-    score: 0,
-    id: 1598075378858,
-  },
-  {
-    title: "The storm is over",
-    name: "Kelly",
-    style: "R&B",
-    length: "4:34",
-    picuture: "storm.jpg",
-    score: 0,
-    id: 1598075381095,
-  },
-  {
-    title: "There is no excuse my friend",
-    name: "Michael Learns",
-    style: "rock",
-    length: "3:34",
-    picuture: "heart.png",
-    score: 0,
-    id: 1598075616397,
-  },
-];
+let songs = [];
 
 const formEl = document.querySelector('.form-submit');
 const tableBody = document.querySelector('.tbody');
@@ -45,11 +19,12 @@ const filterByStyle = document.querySelector('[name="searchStyle"]');
 const reset = document.querySelector('.reset');
 const searchs = document.querySelectorAll(".search");
 
+//function to show the list of the songs
 const songsLists = () => {
   const html = songs.map(song => 
   `<tr>
     <td>
-      <img src="${song.picuture}" alt="artist's image" width="5rem" height="5rem">
+      <img src="${song.picuture}" alt="artist's image">
     </td>
     <td>
       <span>Song's name:</span>
@@ -73,6 +48,7 @@ const songsLists = () => {
   tableBody.innerHTML = html;
 }
 
+//This function takes values of the inputs form the users and output it.
 const generatesListsOfSongs = (e) => {
   e.preventDefault();
   const form = e.currentTarget;
@@ -86,19 +62,17 @@ const generatesListsOfSongs = (e) => {
     id: Date.now(),
   };
   songs.push(newSongs);
+  form.reset();
   tableBody.dispatchEvent(new CustomEvent('listsSongsUpdated'));
 };
 
-const filterList = () => {
-  const filteredByName = songs.filter(song => song.title.value);
-  console.log(filteredByName);
-};
-
+//Function to delete songs
 const deleteSongs = (id) => {
   songs = songs.filter(song => song.id !== id);
   tableBody.dispatchEvent(new CustomEvent('listsSongsUpdated'));
 };
 
+//Fuction to update scores
 const updateSocres = (id) => {
   let updatedSong = songs.find(song => song.id === id);
   updatedSong.score++;
@@ -108,22 +82,90 @@ const updateSocres = (id) => {
   });
   tableBody.dispatchEvent(new CustomEvent('listsSongsUpdated'));
 };
-const searchFunction = (e) => {
-  const textSearched = e.target.value;
-  let filteringSons = [...songs]
-  songs = songs.filter(song => song.title.includes(textSearched));
-  songs = songs.filter(song => song.style.includes(textSearched));
-  tableBody.dispatchEvent(new CustomEvent('listsSongsUpdated'));
-}
 
+//listen for inputs and take the value and compare to the litle of lists of the songs
+filterByName.addEventListener("input", (e) => {
+  const textSearched = e.target.value.toLocaleLowerCase();
+  let songsFilteredByTitle = songs.filter(song => song.title.toLocaleLowerCase().includes(textSearched));
+  const html = songsFilteredByTitle.map(song => 
+  `<tr>
+    <td>
+      <img src="${song.picuture}" alt="artist's image">
+    </td>
+    <td>
+      <span>Song's name:</span>
+      <span>${song.style}</span>
+    </td>
+    <td>
+      <span>Artist name:</span>
+      <span>${song.name}</span>
+    </td>
+    <td>
+      <span>Score:</span>
+      <span>${song.score}</span>
+    </td>
+    <td>
+      <button type="button" value="${song.id}" class="update">+1</button>
+    </td>
+    <td>
+      <button type="button" value="${song.id}" class="delete">Delete</button>
+    </td>
+  </tr>`).join('');
+  tableBody.innerHTML = html;
+});
+
+//listen for inputs and take the value and compare to the style of lists of the songs
+filterByStyle.addEventListener("input", e => {
+  const textSearched = e.target.value.toLocaleLowerCase();
+  let songsFilteredByStyle = songs.filter(song => song.style.toLocaleLowerCase().includes(textSearched));
+  const html = songsFilteredByStyle.map(song => 
+  `<tr>
+    <td>
+      <img src="${song.picuture}" alt="artist's image">
+    </td>
+    <td>
+      <span>Song's name:</span>
+      <span>${song.style}</span>
+    </td>
+    <td>
+      <span>Artist name:</span>
+      <span>${song.name}</span>
+    </td>
+    <td>
+      <span>Score:</span>
+      <span>${song.score}</span>
+    </td>
+    <td>
+      <button type="button" value="${song.id}" class="update">+1</button>
+    </td>
+    <td>
+      <button type="button" value="${song.id}" class="delete">Delete</button>
+    </td>
+  </tr>`).join('');
+  tableBody.innerHTML = html;
+});
+
+//reset the search inputs
 const resetFunction = (e) => {
-  searchs.value = '';
-  songs
+  filterByName.value = '',
+  filterByStyle.value = '',
+  tableBody.dispatchEvent(new CustomEvent('listsSongsUpdated'));
+};
+
+const initToLocalStorage = () => {
+  localStorage.setItem('songs', JSON.stringify(songs));
+};
+
+const restoreFromLocalStorage = () => {
+  const listOfSongs = JSON.parse(localStorage.getItem('songs'));
+  if (listOfSongs.length) {
+    songs.push(...listOfSongs);
+  }
   tableBody.dispatchEvent(new CustomEvent('listsSongsUpdated'));
 }
 
+//Here the listeners events
 formEl.addEventListener("submit", generatesListsOfSongs);
-searchs.forEach(search => search.addEventListener("input", searchFunction));
 reset.addEventListener("click", resetFunction);
 tableBody.addEventListener("click", (e) => {
   const id = Number(e.target.value);
@@ -131,7 +173,6 @@ tableBody.addEventListener("click", (e) => {
     deleteSongs(id);
   }
 });
-
 tableBody.addEventListener("click", (e) => {
   const id = Number(e.target.value);
   if (e.target.matches('.update')) {
@@ -140,3 +181,5 @@ tableBody.addEventListener("click", (e) => {
 })
 
 tableBody.addEventListener('listsSongsUpdated', songsLists);
+tableBody.addEventListener('listsSongsUpdated', initToLocalStorage);
+restoreFromLocalStorage();
